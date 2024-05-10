31af3f082ca341df421ae0adafdf1a0d303522c8
Vinicius Mayrink <vmayrink12@gmail.com>
2024-05-10 14:04:27 -0300

    Separando rotas em arquivos por contexto

---
+++ src/routes/ingrediente.py
@@ -0,0 +1,48 @@
+from flask import jsonify, request
+from models.ingrediente import Ingrediente
+from marshmallow import ValidationError
+from schemas.ingrediente import IngredienteCreationSchema
+from flask import Blueprint
+
+
+def build_routes(session):
+    bp = Blueprint('bp', __name__)
+
+    @bp.get("/ingrediente")
+    def get_ingredientes():
+        ingredientes = session.query(Ingrediente).all()
+        return jsonify([i.serialize() for i in ingredientes])
+
+    @bp.get("/ingrediente/<int:id>")
+    def get_ingrediente_by_id(id: int):
+        ingrediente = session.query(Ingrediente).filter(
+                Ingrediente.id == id).first()
+        return jsonify(ingrediente.serialize())
+
+    @bp.post("/ingrediente")
+    def set_ingrediente():
+        try:
+            request_data = IngredienteCreationSchema().load(request.json)
+        except ValidationError as err:
+            return jsonify(err.messages), 400
+
+        nome = request_data["nome"]
+        descricao = request_data["descricao"]
+        ingrediente = Ingrediente(nome=nome, descricao=descricao)
+        session.add(ingrediente)
+        session.commit()
+        session.refresh(ingrediente)
+        return jsonify(ingrediente.serialize())
+
+    @bp.delete("/ingrediente/<int:id>")
+    def delete_ingrediente(id: int):
+        ingrediente = session.query(Ingrediente).filter(
+                Ingrediente.id == id).first()
+        if not ingrediente:
+            return "Ingrediente nao encontrado", 404
+
+        session.delete(ingrediente)
+        session.commit()
+        return jsonify(ingrediente.serialize())
+
+    return bp
